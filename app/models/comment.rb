fd600f0c3f6877f64cbbd09d46af063b2012c08b
Ha Lee <halee20@users.noreply.github.com>
2016-11-30 12:14:50 -0600

    3
    3

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+class Comment < ActiveRecord::Base
+
+  belongs_to :photo
+
+  belongs_to :user
+
+  validates :user_id, :presence => true
+
+  validates :photo_id, :presence => true
+
+  validates :body, :presence => true
+
+end
